b33abd77da9e74a84c0584a242a4333d9a089405
Kelvin Lam <kelvin7039@gmail.com>
2017-12-07 11:59:46 +0100

    added linux defines

--- Include/std.h
+++ Include/std.h
@@ -1,10 +1,23 @@
 #pragma once
 
-#ifdef GameEngine3D_EXPORTS
-# define SE_API __declspec(dllexport)
+#ifdef _WIN32 || __CYGWIN__
+# define SE_EXPORT __declspec(dllexport)
+# define SE_IMPORT __declspec(dllimport)
 #else
-# define SE_API __declspec(dllimport)
+# if __GNUC__ >= 4
+#  define SE_EXPORT __attribute__ ((visibility ("default")))
+#  define SE_IMPORT __attribute__ ((visibility ("default")))
+# else
+#  define SE_EXPORT
+#  define SE_IMPORT
+# endif
 #endif
+
+#ifdef GameEngine3D_EXPORTS
+# define SE_API SE_EXPORT
+#else
+# define SE_API SE_IMPORT
+# endif
 
 #include <string>
 #include <functional>
