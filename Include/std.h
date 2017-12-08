1ec727518966dcdad4b7a92dc3895f4923955123
lamram <kelvin7039@gmail.com>
2017-12-08 01:33:29 +0100

    removed os check from std

--- Include/std.h
+++ Include/std.h
@@ -1,13 +1,5 @@
 #pragma once
 
-#ifdef _WIN32 || __CYGWIN__
-# include <Windows.h>
-#else
-# ifdef __linux__
-#  include <X11/Xlib.h>
-# endif
-#endif
-
 #include <string>
 #include <functional>
 #include <map>
